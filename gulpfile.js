12516a56feb2515a6fe0bfda53c2509271e08185
Sandra <lissetdovah@gmail.com>
2020-11-07 22:38:35 -0600

    Prueba con Gulp (check dest)

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,26 @@
-const gulp = require("gulp"),
+const gulp = require('gulp'),
     babelify = require('babelify'),
-    browserify = require("browserify")
-    fs = require("fs");
+    browserify = require('browserify')
+    //fs = require("fs");
 ;
 
-gulp.task("bundle", function(done){
-    browserify({
+gulp.task('bundle', function(done) {
+    gulp.watch("*.js", function() {
+        browserify({
         plugin: 
             [ [require('esmify')] ]
         })
         .transform(babelify)
-        .require("./script.js", { entry: true })
+        .require('./script.js', { 
+            entry: true
+        })
         .bundle()
-        .on("error", function (err) { console.log("Error: " + err.message); })
-        .pipe(fs.createWriteStream("bundle.js"));
-
+        .on('error', function (err) { 
+            console.log('Error: ' + err.message); 
+        })
+        //.pipe(gulp.src('script.js')
+        .pipe(gulp.dest('./bundle.js'));
+        //.pipe(fs.createWriteStream("bundle.js"));
+	});
     done();
 });
